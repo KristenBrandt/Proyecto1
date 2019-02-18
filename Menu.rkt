99aeb0e9146663e66d938d82d3ef127637445588
Darktone <42974480+Jos260400@users.noreply.github.com>
2019-02-17 21:12:03 -0600

    Add files via upload

---
+++ Menu.rkt
@@ -0,0 +1,21 @@
+#|
+  Fernando Garavito 18071
+  Javier Cospin 18099
+  Kristen Brandt 171482
+|#
+
+(print "Bienvenido!!")
+(set primero (read))
+(set segundo (read))
+(Escoge = 0)
+(print "1. Sumar")
+(print "2. Restar")
+(print "3. Multiplicar")
+(print "4. Dividir")
+(print "5. Salir")
+
+(if(Escoge == 1)(print( + primero segundo )))
+(if(Escoge == 2)(print( - primero segundo )))
+(if(Escoge == 3)(print( * primero segundo )))
+(if(Escoge == 4)(print( /  primero segundo )))
+(if(Escoge == 5)(print( Adiós, vuelve pronto )))
